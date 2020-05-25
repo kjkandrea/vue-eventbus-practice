5408e3804adcc07fe2eced6794c4687831129f91
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-05-25 20:52:35 +0900

    컴포넌트간 통신 예제

---
+++ src/Receiver.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Caller />
+  </div>
+</template>
+
+<script>
+import EventBus from "./EventBus"
+import Caller from './components/Caller.vue'
+
+export default {
+  name: 'Receiver',
+  components: {
+    Caller
+  },
+  methods: {
+    Receive(v) {
+      console.log('발신자가 '+v+'라고 말하네요.')
+    }
+  },
+  created(){
+    EventBus.$on('Call', parameter => {
+        this.Receive(parameter)
+    })
+  }
+}
+</script>
---
+++ src/components/Caller.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button @click="call">Receiver에게 말해요.</button>
+  </div>
+</template>
+
+<script>
+import EventBus from "../EventBus"
+
+export default {
+  name: 'Caller',
+  methods: {
+    call(){
+      const argument = '바보';
+      EventBus.$emit('Call', argument);
+    },
+  }
+}
+</script>
